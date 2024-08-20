b26eb5feaeb052b730ab839ba08adf19201ef569
Arpan Gupta <arpan.gupta@lpu.in>
2024-08-20 12:18:04 +0530

    added types.js files

--- todo-app/backend/index.js
+++ todo-app/backend/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { createTODO } = require('./types');
 const app = express();
 
 app.use(express.json());
 
 app.post('/todo',function(req,res){
+    const createPayload = req.body;
+    const parsedPayLoad = createTODO.safeParse(createPayload)
+    if(parsedPayLoad.success){
+        
+    }
     
 })
 app.get('/todos',function(req,res){
