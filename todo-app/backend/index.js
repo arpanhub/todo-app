009ff9380f865a3b26262d8f09d7de52d444f7b4
Arpan Gupta <arpan.gupta@lpu.in>
2024-08-20 12:41:12 +0530

    added routes for complted,show todos

--- todo-app/backend/index.js
+++ todo-app/backend/index.js
@@ -2,24 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { createTODO,updateTODO } = require('./types');
 const app = express();
+const todo = require('./db');
 
 app.use(express.json());
 
-app.post('/todo',function(req,res){
+app.post('/todo', async function(req,res){
     const createPayload = req.body;
     const parsedPayLoad = createTODO.safeParse(createPayload)
-    if(parsedPayLoad.success){
+    if(parsedPayLoad.success){  
         res.status(411).json({
             msg:"you sent wrong data"
         })
         return;
     }
+    await todo.create({
+        title:parsedPayLoad.title,
+        Description:parsedPayLoad.Description,
+        Completed:false
+    })
+    res.json({
+        msg:"TODO created"
+    })
     
 })
-app.get('/todos',function(req,res){
-    
+app.get('/todos',async function(req,res){
+    const todos = await todo.find({});
+    res.json({
+        todos
+    })
 })
-app.put('/completed',function(resq,res){
+app.put('/completed',async function(resq,res){
     const updatePayLoad = req.body;
     const parsedPayLoad = updateTODO.safeParse(updatePayLoad);
     if(!parsedPayLoad.success){
@@ -28,5 +40,13 @@ app.put('/completed',function(resq,res){
         })
         return;
     }
+    await todo.update({
+        _id: req.body.id;
+    },{
+        Completed:true
+    })
+    res.json({
+        msg:"TODO updated"
+    })
 
 })
